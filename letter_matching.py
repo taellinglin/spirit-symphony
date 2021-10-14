d3598b1081e497a57de1121caaacd5cf74ee05fa
taellinglin <82527149+taellinglin@users.noreply.github.com>
2021-10-14 15:32:35 -0500

    got scenes working
    working on scene deconstruction.

---
+++ letter_matching.py
@@ -0,0 +1,34 @@
+import sys
+from stageflow.core import Stage
+
+class LetterMatching(Stage):
+    def __init__(self, exit_stage = "main_menu"):
+        self.exit_stage = exit_stage
+
+    def enter(self, data):
+        base.cam.set_z(10)
+        base.cam.look_at(render)
+        #self.logo()
+        #self.press_start()
+        self.text_begin()
+        #self.glyph_rings = GlyphRings(font=base.loader.load_font('fonts/Daemon.otf'))
+        #self.bgm = BGM()
+        #self.motion_blur = MotionBlur()
+        base.task_mgr.add(self.update)
+        base.accept('escape', sys.exit)
+
+    def text_begin(self):
+        text_begin = base.loader.loadModel("models/text_begin.bam")
+        #text_begin.set_p(90)
+        #text_begin.set_x(-0.55)
+        #text_begin.set_y(-3)
+        #press_start.set_z(-0.7)
+        #press_start.set_scale(0.15,0.15,0.75)
+        text_begin.reparent_to(render)
+
+    def update(self, task):
+        dt = globalClock.get_dt()
+        #self.glyph_rings.update(dt)
+        #base.camera.set_hpr(base.camera, (0.05, 0.05, 0.05))
+        base.cam.look_at(render)
+        return task.cont
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 from direct import showbase
 
 from direct.showbase.ShowBase import ShowBase
+from letter_matching import LetterMatching
 
 
 from stageflow import Flow
@@ -21,11 +22,13 @@ from stageflow import Stage
 from stageflow.panda3d import Cutscene
 
 
-class TitleScreen():
-    def __init__(self):
-        pass
+class TitleScreen(Stage):
+    def __init__(self, exit_stage = 'main_menu'):
+        self.exit_stage = exit_stage
 
     def enter(self, data):
+        self.data = data
+        base.accept("enter", base.flow.transition, [self.exit_stage])
         base.cam.set_z(256)
         base.cam.look_at(render)
         self.logo()
@@ -39,9 +42,9 @@ class TitleScreen():
     def logo(self):
         logo = CardMaker('logo')
         logo.set_frame(0 ,0 ,1,1)
-        imageObject = OnscreenImage(image='graphics/SoulSymphonyLogo.png', pos=(-0.2, 1, 0.5), scale=(1,0.5,0.5))
-        imageObject.setTransparency(TransparencyAttrib.MAlpha)
-        bg2 = render.attach_new_node(logo.generate())
+        self.imageObject = OnscreenImage(image='graphics/SoulSymphonyLogo.png', pos=(-0.2, 1, 0.5), scale=(1,0.5,0.5))
+        self.imageObject.setTransparency(TransparencyAttrib.MAlpha)
+        self.bg2 = render.attach_new_node(logo.generate())
 
     def press_start(self):
         press_start = base.loader.loadModel("models/press_start.bam")
@@ -50,7 +53,7 @@ class TitleScreen():
         press_start.set_y(-3)
         press_start.set_z(-0.7)
         press_start.set_scale(0.15,0.15,0.75)
-        press_start.reparent_to(base.cam2d)
+        self.subtitle = press_start.reparent_to(base.cam2d)
 
     def torus(self):
         press_start = base.loader.loadModel("models/torus.bam")
@@ -63,11 +66,17 @@ class TitleScreen():
 
     def update(self, task):
         dt = globalClock.get_dt()
-        self.glyph_rings.update(dt)
+        if self.glyph_rings:
+            self.glyph_rings.update(dt)
         base.camera.set_hpr(base.camera, (0.05, 0.05, 0.05))
         base.cam.look_at(render)
         return task.cont
 
+    def exit(self, data):
+        self.subtitle.detachNode()
+        self.glyph_rings.detachNode()
+        self.bg2.detachNode()
+        return data
 
 class Base(ShowBase):
     def __init__(self):
@@ -82,7 +91,8 @@ base = Base()
 base.flow = Flow(
     stages=dict(
         splash=Panda3DSplash(exit_stage='title_screen'),
-        title_screen=TitleScreen(),
+        title_screen=TitleScreen(exit_stage='letter_matching'),
+        letter_matching=LetterMatching(exit_stage='quit'),
         quit=Quit()
     ),
     initial_stage = 'splash',

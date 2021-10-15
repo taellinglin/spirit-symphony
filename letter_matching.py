180381ae97c43f7ed047303222f6712a163a3263
taellinglin <82527149+taellinglin@users.noreply.github.com>
2021-10-14 22:14:30 -0500

    working on letter matching game
    Letter matching game!

--- letter_matching.py
+++ letter_matching.py
@@ -1,5 +1,8 @@
 import sys
 from stageflow.core import Stage
+from random import randint, choice
+from panda3d.core import TextNode
+from panda3d.core import TextFont
 
 class LetterMatching(Stage):
     def __init__(self, exit_stage = "main_menu"):
@@ -11,11 +14,13 @@ class LetterMatching(Stage):
         #self.logo()
         #self.press_start()
         self.text_begin()
+        self.show_letter(str(choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')), base.loader.load_font('fonts/Daemon.otf'))
         #self.glyph_rings = GlyphRings(font=base.loader.load_font('fonts/Daemon.otf'))
         #self.bgm = BGM()
         #self.motion_blur = MotionBlur()
         base.task_mgr.add(self.update)
         base.accept('escape', sys.exit)
+        #base.accept('enter', self.enter(data))
 
     def text_begin(self):
         text_begin = base.loader.loadModel("models/text_begin.bam")
@@ -25,6 +30,14 @@ class LetterMatching(Stage):
         #press_start.set_z(-0.7)
         #press_start.set_scale(0.15,0.15,0.75)
         text_begin.reparent_to(render)
+    
+    def show_letter(self, letter, font):
+        self.letter = TextNode('glyph_'+letter)
+        self.font = font
+        self.letter.text = letter
+        self.letter.font = font
+        letterbox = render.attach_new_node(self.letter)
+        letterbox.set_scale(5,5,5)
 
     def update(self, task):
         dt = globalClock.get_dt()

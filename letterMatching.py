33fbe262641e7a61716f5a4a45aaadfcd8379e0d
taellinglin <82527149+taellinglin@users.noreply.github.com>
2021-10-15 13:29:28 -0500

    Working on Classes
    The Classes LetterMatching and TitleScreen use base.*** and it does not have a reference to ShowBase()

--- letterMatching.py
+++ letterMatching.py
@@ -4,6 +4,9 @@ from random import randint, choice
 from panda3d.core import TextNode
 from panda3d.core import TextFont
 
+from stageflow import Stage
+from stageflow.panda3d import Cutscene
+
 class LetterMatching(Stage):
     def __init__(self, exit_stage = "main_menu"):
         self.exit_stage = exit_stage
@@ -11,37 +14,37 @@ class LetterMatching(Stage):
     def enter(self, data):
         base.cam.set_z(10)
         base.cam.look_at(render)
-        #self.logo()
-        #self.press_start()
         self.text_begin()
-        self.show_letter(str(choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')), base.loader.load_font('fonts/Daemon.otf'))
-        #self.glyph_rings = GlyphRings(font=base.loader.load_font('fonts/Daemon.otf'))
-        #self.bgm = BGM()
-        #self.motion_blur = MotionBlur()
+        self.show_letter()
         base.task_mgr.add(self.update)
         base.accept('escape', sys.exit)
-        #base.accept('enter', self.enter(data))
+        base.accept('enter', self.next_letter())
 
     def text_begin(self):
         text_begin = base.loader.loadModel("models/text_begin.bam")
         text_begin.set_p(90)
-        #text_begin.set_x(-0.55)
-        #text_begin.set_y(-3)
-        #press_start.set_z(-0.7)
-        #press_start.set_scale(0.15,0.15,0.75)
         text_begin.reparent_to(render)
     
-    def show_letter(self, letter, font):
+    def show_letter(self, letter = str(choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')), font = base.loader.load_font('fonts/Daemon.otf')):
         self.letter = TextNode('glyph_'+letter)
         self.font = font
         self.letter.text = letter
         self.letter.font = font
-        letterbox = render.attach_new_node(self.letter)
-        letterbox.set_scale(5,5,5)
+        self.letterbox = render.attach_new_node(self.letter)
+        self.letterbox.set_scale(5,5,5)
+
+    def next_letter(self, letter = str(choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')), font = base.loader.load_font('fonts/Daemon.otf')):
+        if self.letterbox:
+            self.letterbox.detachNode()
+        self.letter = TextNode('glyph_'+letter)
+        self.font = font
+        self.letter.text = letter
+        self.letter.font = font
+        self.letterbox = render.attach_new_node(self.letter)
+        self.letterbox.set_scale(5,5,5)
+
 
     def update(self, task):
         dt = globalClock.get_dt()
-        #self.glyph_rings.update(dt)
-        #base.camera.set_hpr(base.camera, (0.05, 0.05, 0.05))
         base.cam.look_at(render)
         return task.cont
